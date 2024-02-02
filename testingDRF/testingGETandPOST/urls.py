3daf1b3412b4c31873fe1795ac6c5574b291ff61
Ali Daraev <nevermore09100910@gmail.com>
2024-02-02 19:33:55 +0600

    Добавлена функция добавления записи в бд с помощью json

--- testingDRF/testingGETandPOST/urls.py
+++ testingDRF/testingGETandPOST/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ConverterRetrieveAPIView
+from .views import ConverterRetrieveAPIView, ConverterCreateAPIView
 
 urlpatterns = [
-    path('api/retreive/<int:id>/', ConverterRetrieveAPIView.as_view(), name='get-converter'),
+    path('api/retreive/<int:id>/', ConverterRetrieveAPIView.as_view(), name='retreive'),
+    path('api/create/', ConverterCreateAPIView.as_view(), name='create'),
 ]
--- testingDRF/testingGETandPOST/views.py
+++ testingDRF/testingGETandPOST/views.py
@@ -4,10 +4,15 @@ from .serializers import ConverterSerializer
 
 
 from rest_framework.views import APIView
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView, CreateAPIView
 
 
 class ConverterRetrieveAPIView(RetrieveAPIView):
     queryset = Converter.objects.all()
     serializer_class = ConverterSerializer
     lookup_field = 'id'
+
+
+class ConverterCreateAPIView(CreateAPIView):
+    queryset = Converter.objects.all()
+    serializer_class = ConverterSerializer
