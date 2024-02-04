09f8baeeb14e74c24099ca83fe4b3ed2de71e414
Ali Daraev <nevermore09100910@gmail.com>
2024-02-04 19:33:47 +0600

    Добавлена функция вывода всех записей за последний час также время сервера установлено на UTC+6

--- testingDRF/testingGETandPOST/urls.py
+++ testingDRF/testingGETandPOST/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ConverterRetrieveAPIView, ConverterCreateAPIView
+from .views import ConverterRetrieveAPIView, ConverterCreateAPIView, ConverterListAPIView
 
 urlpatterns = [
     path('api/retreive/<int:id>/', ConverterRetrieveAPIView.as_view(), name='retreive'),
     path('api/create/', ConverterCreateAPIView.as_view(), name='create'),
+    path('api/lasthour/', ConverterListAPIView.as_view(), name='lasthour')
 ]
--- testingDRF/testingGETandPOST/views.py
+++ testingDRF/testingGETandPOST/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta
+
 from .models import Converter
 from .serializers import ConverterSerializer
 
-
-from rest_framework.views import APIView
-from rest_framework.generics import RetrieveAPIView, CreateAPIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import RetrieveAPIView, CreateAPIView, ListAPIView
 
 
 class ConverterRetrieveAPIView(RetrieveAPIView):
@@ -16,3 +17,19 @@ class ConverterRetrieveAPIView(RetrieveAPIView):
 class ConverterCreateAPIView(CreateAPIView):
     queryset = Converter.objects.all()
     serializer_class = ConverterSerializer
+
+
+class ConverterListAPIView(ListAPIView):
+    serializer_class = ConverterSerializer
+
+    def get_queryset(self):
+        end_datetime = datetime.now()
+        start_datetime = end_datetime - timedelta(hours=1)
+
+        queryset = Converter.objects.filter(
+            last_update_date=start_datetime.date(),
+            last_update_time__gte=start_datetime.time(),
+            last_update_time__lt=end_datetime.time()
+        )
+
+        return queryset
