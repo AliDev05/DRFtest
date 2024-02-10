75475a7a3e50f17b9692dca01056f4e4b27de614
Ali Daraev <nevermore09100910@gmail.com>
2024-02-11 00:18:39 +0600

    Добавлен вывод всех записей по дате, также как комментарии добавлены иные варианты выполнения кода , на данный момент не реализована функции вывода по времени, ее тестовый вариант лежит как комментарий

--- testingDRF/testingGETandPOST/urls.py
+++ testingDRF/testingGETandPOST/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ConverterRetrieveAPIView, ConverterCreateAPIView, ConverterListAPIView
+from django.urls import re_path
+from .views import ConverterRetrieveAPIView, ConverterCreateAPIView, DateAPIView, TimeAPIView
 
 urlpatterns = [
     path('api/retreive/<int:id>/', ConverterRetrieveAPIView.as_view(), name='retreive'),
     path('api/create/', ConverterCreateAPIView.as_view(), name='create'),
-    path('api/lasthour/', ConverterListAPIView.as_view(), name='lasthour')
+    re_path(r'^api/date/(?P<datedigits>\d{1,3}[wd])/$', DateAPIView.as_view()),
+    re_path(r'^api/time/(?P<timedigits>\d{1,3}[hms])/$', TimeAPIView.as_view()),
 ]
--- testingDRF/testingGETandPOST/views.py
+++ testingDRF/testingGETandPOST/views.py
@@ -5,7 +5,9 @@ from .serializers import ConverterSerializer
 
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.generics import RetrieveAPIView, CreateAPIView, ListAPIView
+
 
 
 class ConverterRetrieveAPIView(RetrieveAPIView):
@@ -19,17 +21,123 @@ class ConverterCreateAPIView(CreateAPIView):
     serializer_class = ConverterSerializer
 
 
-class ConverterListAPIView(ListAPIView):
+
+
+class DateAPIView(APIView):
+    serializer_class = ConverterSerializer
+
+    def get(self, request, datedigits):
+        end_date = datetime.now().date()
+
+        num = int(datedigits[:len(datedigits)-1])
+        let = datedigits[-1]
+
+        if let == 'w':
+            start_date = end_date - timedelta(weeks=num)
+        else :
+            start_date = end_date - timedelta(days=num)
+
+
+
+        queryset = Converter.objects.filter(
+            last_update_date__gte=start_datetime,
+            last_update_date__lte=end_datetime,
+        )
+
+
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+
+"""class TimeAPIView(APIView):
+    serializer_class = ConverterSerializer
+
+    def get(self, request, timedigits):
+        end_time = datetime.now()
+
+        num = int(timedigits[:len(timedigits)-1])
+        let = timedigits[-1]
+
+        if let == 'h':
+            start_time = end_time - timedelta(hours=num)
+        elif let == 'm' :
+            start_time = end_time - timedelta(minutes=num)
+        else :
+            start_time = end_time - timedelta(seconds=num)
+
+        print(start_time)
+        print(end_time)
+
+
+        queryset = Converter.objects.filter(
+            last_update_time__gte=start_time,
+            last_update_time__lte=end_time
+        )
+
+
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+"""
+
+
+
+"""class DateTimeAPIView(ListAPIView):
     serializer_class = ConverterSerializer
 
     def get_queryset(self):
+        datetimedigits = self.kwargs['datetime']
         end_datetime = datetime.now()
-        start_datetime = end_datetime - timedelta(hours=1)
 
+        num = int(datetimedigits[:len(datetimedigits)-1])
+        let = datetimedigits[-1]
+
+        
+
+
+        if let == 'w':
+            start_datetime = end_datetime - timedelta(weeks=num)
+        elif let == 'd':
+            start_datetime = end_datetime - timedelta(days=num)
+        elif let == 'h':
+            start_datetime = end_datetime - timedelta(hours=num)
+        elif let == 'm':
+            start_datetime = end_datetime - timedelta(minutes=num)
+        else :
+            start_datetime = end_datetime - timedelta(seconds=num)
+
+            
         queryset = Converter.objects.filter(
-            last_update_date=start_datetime.date(),
+            last_update_date__gte=start_datetime.date(),
+            last_update_date__lt=end_datetime.date(),
             last_update_time__gte=start_datetime.time(),
             last_update_time__lt=end_datetime.time()
         )
 
         return queryset
+
+"""
+
+
+
+
+#from dateutil.relativedelta import relativedelta
+
+"""class DateFilterAPIView(ListAPIView):
+    serializer_class = ConverterSerializer
+
+    def get_queryset(self):
+        datedigits = self.kwargs['datedigits']
+        year = int(datedigits[:2])
+        month = int(datedigits[2:4])
+        day = int(datedigits[4:6])
+
+        end_date = datetime.now().date()
+        date_to_subtract = relativedelta(years=year, months=month, days=day)
+        start_date = end_date - date_to_subtract
+
+        queryset = Converter.objects.filter(
+            last_update_date__gte=start_date,
+            last_update_date__lt=end_date
+        )
+        return queryset
+"""
